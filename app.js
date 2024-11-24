732e039a4686a876f82ab513e694f4dcdde77180
Jose Peanilla <josepeanilla@hotmail.com>
2024-11-24 18:13:33 +0100

    Preparando el servidor

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API de lista de tareas');
+});
+
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
